1370d928390de3fd10d433746bc9a024b1f882a3
fahim <fahimahamedweb@gmail.com>
2025-01-12 12:38:04 +0600

    fixded role login issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,18 @@ return Application::configure(basePath: dirname(__DIR__))
 
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware(['web', 'auth', 'role:admin'])
+                ->prefix('admin')
+                ->as('admin.')
+                ->group(base_path('routes/admin.php'));
+
+            Route::middleware(['web', 'auth', 'role:vendor'])
+                ->prefix('vendor')
+                ->as('vendor.')
+                ->group(base_path('routes/vendor.php'));
+
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
 
